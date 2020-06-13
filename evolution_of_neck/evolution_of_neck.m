d0359f8683c83f1db81e105f996fb0e5b5476be1
mirsandiharyo <mirsandiharyo@gmail.com>
2020-06-13 16:20:55 +0200

    Cleaning

--- evolution_of_neck/evolution_of_neck.m
+++ evolution_of_neck/evolution_of_neck.m
@@ -1,42 +1,52 @@
 %% 
 % An image processing script to determine the evolution of neck radius 
-% during coalescence of two bubbles or droplets. The script requires 
-% tifflib library. Sample and background images can be found inside the folder.
+% during coalescence of two bubbles or droplets. 
+% Sample and background images can be found inside the folder.
 % Created by: Haryo Mirsandi
 
 addpath('../tifflib/')
 close all;
 clearvars;
 
-%% read background image
-disp('choose the background image');
-[file_name, file_path] = uigetfile('.tif');
+%% read the input file
+disp('choose the input file (.txt)');
+[input_name, file_path] = uigetfile('.txt');
 cd(file_path);
-image_background = imread(file_name);
-image_background = double(image_background);
+fid = fopen(input_name);
+read_line = regexp(fgetl(fid), '= ', 'split');
+bg_name = read_line{2};
+read_line = regexp(fgetl(fid), '= ', 'split');
+series_name = read_line{2};
+read_line = regexp(fgetl(fid), '=', 'split');
+image.dt = str2double(read_line{2});
+read_line = regexp(fgetl(fid), '=', 'split');
+image.res = str2double(read_line{2});
+read_line = regexp(fgetl(fid), '=', 'split');
+image.start = str2double(read_line{2});
+read_line = regexp(fgetl(fid), '=', 'split');
+image.threshold = str2double(read_line{2});
+read_line = regexp(fgetl(fid), '=', 'split');
+image.center_x = str2double(read_line{2});
+read_line = regexp(fgetl(fid), '=', 'split');
+image.center_y = str2double(read_line{2});
+read_line = regexp(fgetl(fid), '=', 'split');
+image.delta = str2double(read_line{2});
+fclose(fid);
 
-%% read the image series
-disp('choose the image series');
-[file_name, file_path] = uigetfile('.tif');
-% get image information
-image.info = imfinfo(file_name);
+%% read the images
+% background image
+image_background = imread(bg_name);
+image_background = double(image_background);
+% image series
+image.info = imfinfo(series_name);
 image.width = image.info.Width;
 image.height = image.info.Height;
 image.length = length(image.info);
 image.allframe = zeros(image.height,image.width,image.length,'uint16');
 image.info = [];
-% parameters related to the image series, etc.
-image.center_x = 288;       % location of the center of coalescence defined 
-image.center_y = 370;       % from the original image (use imagej)
-image.delta = 10;           % width from the center in the x direction
-image.res = 1.5984e-2;      % mm/pixel (use imagej)
-image.dt = 0.1;             % ms
-image.start = 1;
-image.threshold = 2;        % threshold for binarization. 1.5 is usually OK
-                            % 0.8 for images from paraview
 
-%% store the images in allframe
-file_id = tifflib('open',file_name,'r');
+%% store all frames
+file_id = tifflib('open',series_name,'r');
 rows_per_strip = tifflib('getField',file_id,Tiff.TagID.RowsPerStrip);
 for num=1:image.length  
     tifflib('setDirectory',file_id,num-1);
@@ -51,7 +61,7 @@ for num=1:image.length
 end
 tifflib('close',file_id);
 
-%% determine the evolution of the neck formation
+%% determine the neck radius
 neck_length = zeros(1,image.length);
 for num = image.start:image.length
     % get the current frame and perform binarization
@@ -66,7 +76,7 @@ for num = image.start:image.length
     % plot the whole interface
     figure(1),plot(boundary.ori(:,1),boundary.ori(:,2),'.')
     axis equal
-    % calculate the neck length
+    % calculate the neck radius
     boundary.temp = boundary.ori;
     boundary.temp(boundary.temp(:,2)<(image.center_x-image.delta),:)=[];
     boundary.temp(boundary.temp(:,2)>(image.center_x+image.delta),:)=[];
@@ -79,7 +89,7 @@ for num = image.start:image.length
     neck_length(num) = boundary.min-boundary.max;
 end
 
-%% plot the evolution of neck formation
+%% plot the evolution of neck radius
 neck_length = neck_length*image.res;
 time = 0:image.dt:(image.length-1)*image.dt;
 figure(3), plot(time,neck_length,'LineWidth',3,'Color','r');
